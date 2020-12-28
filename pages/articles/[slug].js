aebfcf455c071ff413cffa43517cd7d9ed975389
cmihad@gmail.com <cmihad@gmail.com>
2020-12-28 11:02:20 +0600

    made some corrections

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -1,45 +1,45 @@
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+// import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-let client = require('contentful').createClient({
-  space: process.env.NEXT_CONTENTFUL_SPACE_ID,
-  accessToken:process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
-});
-export async function getStaticProps(){
-  let data = await client.getEntries({
-    content_type:'articles'
-  })
-  return{
-    paths: data.items.map((item) =>({
-      param: {slug: item.fields.slug},
-    })),
-    fallback: true,
-  };
-}
-export async function getStaticProps( params ){
+// let client = require('contentful').createClient({
+//   space: process.env.NEXT_CONTENTFUL_SPACE_ID,
+//   accessToken:process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
+// });
+// // export async function getStaticProps(){
+// //   let data = await client.getEntries({
+// //     content_type:'articles'
+// //   })
+//   return{
+//     paths: data.items.map((item) =>({
+//       param: {slug: item.fields.slug},
+//     })),
+//     fallback: true,
+//   };
 
-  let data =await client.getEntries({
-    content_type:'articles',
-    "fields.slug": params.slug,
-  })
-return{
+// export async function getStaticProps( params ){
 
-  props: {
-    article : data.items[0]
-  }
-}
-}
+//   let data =await client.getEntries({
+//     content_type:'articles',
+//     "fields.slug": params.slug,
+//   })
+// return{
 
-export default function Articles( articles ){
+//   props: {
+//     article : data.items[0]
+//   }
+// }
+// }
 
-  return(
+// export default function Articles( articles ){
 
-    <div> 
-     <h1>{articles.fields.items}</h1>
-     <div>
-       {documentToReactComponents(articles.fields.content)}
-     </div>
+//   return(
+
+//     <div> 
+//      <h1>{articles.fields.items}</h1>
+//      <div>
+//        {documentToReactComponents(articles.fields.content)}
+//      </div>
     
-    </div>
-  ) 
+//     </div>
+//   ) 
 
-}
+// }
